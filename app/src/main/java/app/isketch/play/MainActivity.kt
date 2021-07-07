6a99f883dd75ee7f467dc5ef5eee0e853c8056f7
Dylan Bickerstaff <dylan@belowaverage.org>
2021-07-06 22:44:22 -0400

    Basic App

--- app/src/main/java/app/isketch/play/MainActivity.kt
+++ app/src/main/java/app/isketch/play/MainActivity.kt
@@ -1,20 +1,34 @@
 package app.isketch.play
 
-import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var MainWV = findViewById<WebView>(R.id.Main_WV)
+        MainWV.webViewClient = WVCOverride()
         var wvSettings = MainWV.settings
         wvSettings.javaScriptEnabled = true
         wvSettings.domStorageEnabled = true
         wvSettings.mediaPlaybackRequiresUserGesture = false;
         MainWV.loadUrl(getString(R.string.app_start_url))
 
+    }
+}
+class WVCOverride : WebViewClient() {
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        view?.isVisible = false
+        super.onPageStarted(view, url, favicon)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        view?.isVisible = true
+        super.onPageFinished(view, url)
     }
 }
